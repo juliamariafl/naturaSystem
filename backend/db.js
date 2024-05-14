f6cb7d370023c41145a6a8c1b7a958986fa41d0e
juliamariafl <julia.leite8@aluno.senai.br>
2024-05-14 16:00:50 -0300

    backend atualizado

--- backend/db.js
+++ backend/db.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-const mysql = require("mysql2");
-
-// Configuração das credenciais de acesso ao banco de dados
-const dbConfig = {
-  host: "localhost", // Host do banco de dados
-  user: "root", // Usuário do banco de dados
-  password: "Desenvolvedor@123", // Senha do banco de dados
-  database: "sistemanatura", // Nome do banco de dados
-};
-
-// Criação da conexão com o banco de dados
-const connection = mysql.createConnection(dbConfig);
-
-// Verifica se a conexão foi bem-sucedida
-connection.connect((err) => {
-  if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err);
-    return;
-  }
-  console.log("Conexão bem-sucedida ao banco de dados!");
-});
-
-module.exports = connection;
-=======
 const mysql = require('mysql2');
 
 // Configuração das credenciais de acesso ao banco de dados
@@ -30,7 +5,7 @@ const dbConfig = {
   host: 'localhost', // Host do banco de dados
   user: 'root', // Usuário do banco de dados
   password: 'Desenvolvedor@123', // Senha do banco de dados
-  database: 'aula' // Nome do banco de dados
+  database: 'sistemaNatura' // Nome do banco de dados
 };
 
 // Criação da conexão com o banco de dados
@@ -46,4 +21,3 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
->>>>>>> ce00ef35239bbd4f4109c01cef58b8c0a7951a5a
